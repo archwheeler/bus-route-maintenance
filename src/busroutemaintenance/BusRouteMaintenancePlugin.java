34c689e4b00a0a86ed916298040779475cf8b6c7
Archie Wheeler <archiewheeler@pm.me>
2020-11-24 16:12:14 +0000

    Create plugin entry point

---
+++ src/busroutemaintenance/BusRouteMaintenancePlugin.java
@@ -0,0 +1,13 @@
+package busroutemaintenance;
+
+import org.openstreetmap.josm.plugins.Plugin;
+import org.openstreetmap.josm.plugins.PluginInformation;
+
+public class BusRouteMaintenancePlugin extends Plugin {
+  
+  public BusRouteMaintenancePlugin(PluginInformation info) {
+    super(info);
+    System.out.println("Foo");
+  }
+  
+}
