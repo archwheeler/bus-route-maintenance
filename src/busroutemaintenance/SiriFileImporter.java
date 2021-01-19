6c9b51592c5e7f5cf4df18337175aa78e2136dd1
Archie Wheeler <archiewheeler@pm.me>
2021-01-19 13:01:51 +0000

    Create a new layer for each imported SIRI file
    The layer contains a waypoint for each latitude/longitude pair.

--- src/busroutemaintenance/SiriFileImporter.java
+++ src/busroutemaintenance/SiriFileImporter.java
@@ -4,8 +4,16 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 
 import java.io.File;
 
+import javax.swing.JOptionPane;
+
 import org.openstreetmap.josm.actions.ExtensionFileFilter;
+import org.openstreetmap.josm.data.gpx.GpxData;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.gui.io.importexport.FileImporter;
+import org.openstreetmap.josm.gui.layer.GpxLayer;
+import org.openstreetmap.josm.gui.progress.ProgressMonitor;
+import org.openstreetmap.josm.gui.util.GuiHelper;
+import org.openstreetmap.josm.io.IllegalDataException;
 
 public class SiriFileImporter extends FileImporter {
 
@@ -20,6 +28,29 @@ public class SiriFileImporter extends FileImporter {
   @Override
   public boolean acceptFile(File pathname) {
     return super.acceptFile(pathname) && SiriFileReader.isSiriFile(pathname);
+  }
+  
+  @Override
+  public void importData(File file, ProgressMonitor monitor) {
+    try {
+      monitor.beginTask(String.format(tr("Importing SIRI file ''%s''..."), file.getName()));
+      
+      SiriFileReader r = new SiriFileReader(file);
+      GpxData gpxData = r.toGpx();
+      monitor.worked(1);
+      
+      GpxLayer gpxLayer = new GpxLayer(gpxData, file.getName());
+      monitor.worked(1);
+      
+      MainApplication.getLayerManager().addLayer(gpxLayer);
+      monitor.worked(1);
+    } catch (IllegalDataException e) {
+      GuiHelper.runInEDT(() -> JOptionPane.showMessageDialog(null,
+          String.format(tr("Error loading SIRI file ''%s'':\n%s"), file.getName(),
+          e.getMessage()), tr("Error"), JOptionPane.WARNING_MESSAGE));
+    } finally {
+      monitor.finishTask();
+    }
   }
   
 }
--- src/busroutemaintenance/SiriFileReader.java
+++ src/busroutemaintenance/SiriFileReader.java
@@ -1,12 +1,23 @@
 package busroutemaintenance;
 
+import static org.openstreetmap.josm.tools.I18n.tr;
+
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.nio.ByteBuffer;
+
+import org.openstreetmap.josm.data.coor.LatLon;
+import org.openstreetmap.josm.data.gpx.GpxData;
+import org.openstreetmap.josm.data.gpx.WayPoint;
+import org.openstreetmap.josm.io.IllegalDataException;
 
 public class SiriFileReader {
   
   private static int SIGNATURE = 0x53495249;
+  private static int SIGNATURE_SIZE = 8;
+  private static int BUFFER_SIZE = 24;
   
   private File file;
   
@@ -21,6 +32,30 @@ public class SiriFileReader {
     } catch (Exception e) {
       return false;
     }
+  }
+
+  public GpxData toGpx() throws IllegalDataException {
+    if (file == null || !isSiriFile(file))
+      throw new IllegalDataException(tr("File is not a SIRI file"));
+    
+    GpxData gpxData = new GpxData();
+    try (BufferedInputStream is = new BufferedInputStream(new FileInputStream(file))) {
+      // Ignore the header information
+      is.skip(SIGNATURE_SIZE);
+      
+      WayPoint wpt;
+      ByteBuffer buffer;
+      byte[] bytes = new byte[BUFFER_SIZE];
+      while(is.read(bytes) != -1) {
+        buffer = ByteBuffer.wrap(bytes);
+        buffer.getInt(); // TODO: do we need time?
+        wpt = new WayPoint(new LatLon(buffer.getDouble(), buffer.getDouble()));
+        gpxData.addWaypoint(wpt);
+      }
+    } catch (Exception e) {
+      throw new IllegalDataException(tr("Could not convert SIRI file to GPX"));
+    }
+    return gpxData;
   }
   
 }
