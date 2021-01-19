46a067d1d53461345b2938a3ac38a5c802841a72
Archie Wheeler <archiewheeler@pm.me>
2021-01-19 16:07:02 +0000

    Create tracks from GPX waypoints
    The current implementation creates a track between each adjacent
    waypoint in the SIRI file. This is both slow and incorrectly connects
    waypoints occuring at vastly different times.

--- src/busroutemaintenance/SiriFileReader.java
+++ src/busroutemaintenance/SiriFileReader.java
@@ -8,9 +8,17 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
 
 import org.openstreetmap.josm.data.coor.LatLon;
 import org.openstreetmap.josm.data.gpx.GpxData;
+import org.openstreetmap.josm.data.gpx.GpxTrack;
+import org.openstreetmap.josm.data.gpx.GpxTrackSegment;
+import org.openstreetmap.josm.data.gpx.IGpxTrackSegment;
 import org.openstreetmap.josm.data.gpx.WayPoint;
 import org.openstreetmap.josm.gui.progress.ProgressMonitor;
 import org.openstreetmap.josm.io.IllegalDataException;
@@ -45,19 +53,37 @@ public class SiriFileReader {
       // Ignore the header information
       is.skip(SIGNATURE_SIZE);
       
-      WayPoint wpt;
+      WayPoint current = null;
+      WayPoint previous = null;
+      List<IGpxTrackSegment> segments = new ArrayList<IGpxTrackSegment>();
       long time;
+      double lat;
+      double lon;
       ByteBuffer buffer;
       byte[] bytes = new byte[BUFFER_SIZE];
       while(is.read(bytes) == BUFFER_SIZE) {
         buffer = ByteBuffer.wrap(bytes);
         buffer.order(ByteOrder.LITTLE_ENDIAN);
+        
         time = (long) buffer.getDouble();
-        wpt = new WayPoint(new LatLon(buffer.getDouble(), buffer.getDouble()));
-        wpt.setTimeInMillis(time);
-        gpxData.addWaypoint(wpt);
+        lat = buffer.getDouble();
+        lon = buffer.getDouble();
+        
+        previous = current;
+        current = new WayPoint(new LatLon(lat, lon));
+        current.setTimeInMillis(time);
+        
+        if (previous != null)
+          segments.add(new GpxTrackSegment(Arrays.asList(previous, current)));
+        
+        gpxData.addWaypoint(current);
         monitor.worked(1);
       }
+      
+      // TODO: assign attributes to track
+      gpxData.addTrack(new GpxTrack(segments, Collections.<String, Object>emptyMap()));
+      monitor.worked(1);
+      
     } catch (Exception e) {
       throw new IllegalDataException(tr("Could not convert SIRI file to GPX"));
     }
