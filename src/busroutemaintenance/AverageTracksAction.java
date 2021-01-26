34c0882861272bef00cb97c3c24941e3cf279b83
Archie Wheeler <archiewheeler@pm.me>
2021-01-26 13:36:28 +0000

    Read GPX data from current layer

--- src/busroutemaintenance/AverageTracksAction.java
+++ src/busroutemaintenance/AverageTracksAction.java
@@ -5,7 +5,14 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 
+import javax.swing.JOptionPane;
+
 import org.openstreetmap.josm.actions.JosmAction;
+import org.openstreetmap.josm.data.gpx.GpxData;
+import org.openstreetmap.josm.gui.layer.GpxLayer;
+import org.openstreetmap.josm.gui.layer.MainLayerManager;
+import org.openstreetmap.josm.gui.util.GuiHelper;
+import org.openstreetmap.josm.gui.MainApplication;
 import org.openstreetmap.josm.tools.Shortcut;
 
 @SuppressWarnings("serial")
@@ -15,13 +22,28 @@ public class AverageTracksAction extends JosmAction {
     super(tr("Average GPX tracks"), "average", tr("Average GPX tracks"),
             Shortcut.registerShortcut("edit:busroutemaintenanceoptions", tr("Edit: {0}",
             tr("Average GPX tracks")), KeyEvent.CHAR_UNDEFINED, Shortcut.NONE), true);
-}
+  }
   
   @Override
   public void actionPerformed(ActionEvent arg0) {
     AverageTracksDialog dlg = new AverageTracksDialog();
+    MainLayerManager layerManager = MainApplication.getLayerManager();
+    
+    // if "Ok" pressed
     if (dlg.getValue() == 1) {
-        System.out.println("Button pressed");
+      GpxLayer currentLayer;
+      GpxData currentData;
+      try {
+        currentLayer = (GpxLayer) layerManager.getActiveLayer();
+        currentData = (GpxData) currentLayer.getData();
+      } catch (Exception e) {
+        GuiHelper.runInEDT(() -> JOptionPane.showMessageDialog(null,
+            tr("Error loading GPX data from current layer."), tr("Error"),
+            JOptionPane.WARNING_MESSAGE));
+        return;
+      }
+      
+      System.out.println(currentData.getTrackSegsCount());
     }
   }
 
