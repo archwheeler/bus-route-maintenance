1e190a58f8e4bf7bce4245c259eb60c3d0de9616
Archie Wheeler <archiewheeler@pm.me>
2021-04-08 12:33:43 +0100

    Add components to the segment dialog
    The user decides whether the GPX track represents a linear or a cyclic
    bus route.

--- src/busroutemaintenance/SegmentTracksDialog.java
+++ src/busroutemaintenance/SegmentTracksDialog.java
@@ -2,15 +2,39 @@ package busroutemaintenance;
 
 import static org.openstreetmap.josm.tools.I18n.tr;
 
+import java.awt.GridBagLayout;
+
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import org.openstreetmap.josm.tools.GBC;
+
 @SuppressWarnings("serial")
 public class SegmentTracksDialog extends BasicDialog {
+  
+  private final JPanel panel = new JPanel(new GridBagLayout());
+  private final JLabel choiceLabel = new JLabel("Is the GPX track linear or cyclic?",
+                                                JLabel.CENTER);
+  private final JRadioButton linearButton = new JRadioButton("Linear");
+  private final JRadioButton cyclicButton = new JRadioButton("Cyclic");
+  private final ButtonGroup choiceButtons = new ButtonGroup();
 
   public SegmentTracksDialog(String layerName) {
-    super(tr("Segment GPX tracks"));
+    super(tr("Segment GPX track"));
     
-    setContent(String.format(
-        tr("Do you want to segment the GPX track in the active layer (''%s'')?"),
-            layerName));
+    panel.add(choiceLabel, GBC.eol().fill(GBC.HORIZONTAL));
+    
+    linearButton.setSelected(true);
+    choiceButtons.add(linearButton);
+    choiceButtons.add(cyclicButton);
+    
+    JPanel buttons = new JPanel(new GridBagLayout());
+    buttons.add(linearButton);
+    buttons.add(cyclicButton, GBC.eol().fill(GBC.HORIZONTAL));
+    panel.add(buttons);
+    
+    setContent(panel);
     
     showDialog();
   }
