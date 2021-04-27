2f93cc65787eced01ee128f51d659e32b2693675
Archie Wheeler <archiewheeler@pm.me>
2021-04-27 14:20:15 +0100

    Make maintenance points clickable
    When a maintenance point is clicked its maintenance is applied to the
    relation.

--- src/busroutemaintenance/gui/MaintenanceLayer.java
+++ src/busroutemaintenance/gui/MaintenanceLayer.java
@@ -5,6 +5,8 @@ import static org.openstreetmap.josm.tools.I18n.tr;
 import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.List;
 
 import javax.swing.Action;
@@ -13,8 +15,10 @@ import javax.swing.Icon;
 import org.openstreetmap.josm.actions.RenameLayerAction;
 import org.openstreetmap.josm.data.Bounds;
 import org.openstreetmap.josm.data.osm.Node;
+import org.openstreetmap.josm.data.osm.Relation;
 import org.openstreetmap.josm.data.osm.visitor.BoundingXYVisitor;
 import org.openstreetmap.josm.gui.MainApplication;
+import org.openstreetmap.josm.gui.MapFrame;
 import org.openstreetmap.josm.gui.MapView;
 import org.openstreetmap.josm.gui.dialogs.LayerListDialog;
 import org.openstreetmap.josm.gui.dialogs.LayerListPopup;
@@ -23,13 +27,21 @@ import org.openstreetmap.josm.tools.ImageProvider;
 
 import busroutemaintenance.Maintenance;
 
-public class MaintenanceLayer extends Layer {
+public class MaintenanceLayer extends Layer implements MouseListener {
+  
+  private static final int SQUARE_SIZE = 32;
+  private static final double DISTANCE_THRESHOLD = 32;
   
   private List<Maintenance> maintenance;
+  private Relation relation;
+  private MapFrame map;
 
-  public MaintenanceLayer(List<Maintenance> maintenance) {
+  public MaintenanceLayer(List<Maintenance> maintenance, Relation relation) {
     super(tr("Maintenance layer"));
     this.maintenance = maintenance;
+    this.relation = relation;
+    this.map = MainApplication.getMap();
+    map.mapView.addMouseListener(this);
     MainApplication.getLayerManager().addLayer(this);
   }
 
@@ -39,7 +51,7 @@ public class MaintenanceLayer extends Layer {
       Node startNode = m.getStartNode();
       Point startPoint = mv.getPoint(startNode);
       g.setColor(Color.RED);
-      g.drawRect(startPoint.x, startPoint.y, 10, 10);
+      g.fillRect(startPoint.x, startPoint.y, SQUARE_SIZE, SQUARE_SIZE);
     }
   }
 
@@ -78,6 +90,43 @@ public class MaintenanceLayer extends Layer {
 
   @Override
   public void visitBoundingBox(BoundingXYVisitor arg0) {
+    return;
+  }
+
+  @Override
+  public void mouseClicked(MouseEvent e) {
+    if (e.getButton() != MouseEvent.BUTTON1) // active layer check?
+      return;
+    Point click = e.getPoint();
+    for (Maintenance m : maintenance) {
+      Point maintenancePoint = map.mapView.getPoint(m.getStartNode());
+      if (click.distance(maintenancePoint) < DISTANCE_THRESHOLD) {
+        m.carryOut(relation);
+        maintenance.remove(m);
+        if (maintenance.size() == 0)
+          map.mapView.removeMouseListener(this);
+        return;
+      }
+    }
+  }
+
+  @Override
+  public void mouseEntered(MouseEvent e) {
+    return;
+  }
+
+  @Override
+  public void mouseExited(MouseEvent e) {
+    return;
+  }
+
+  @Override
+  public void mousePressed(MouseEvent e) {
+    return;
+  }
+
+  @Override
+  public void mouseReleased(MouseEvent e) {
     return;
   }
 
