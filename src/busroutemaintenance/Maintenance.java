7a1b25d897bb81f825063e379de71e453a29141a
Archie Wheeler <archiewheeler@pm.me>
2021-04-27 11:57:36 +0100

    Create Maintenance class
    Each Maintenance object encapsulates a single modification to an
    existing OSM relation.

---
+++ src/busroutemaintenance/Maintenance.java
@@ -0,0 +1,39 @@
+package busroutemaintenance;
+
+import java.util.List;
+import org.openstreetmap.josm.command.ChangeCommand;
+import org.openstreetmap.josm.data.UndoRedoHandler;
+import org.openstreetmap.josm.data.osm.Relation;
+import org.openstreetmap.josm.data.osm.RelationMember;
+import org.openstreetmap.josm.data.osm.Way;
+
+public class Maintenance {
+  
+  private Way start;
+  private Way end;
+  private  List<Way> addWays;
+  
+  public Maintenance(Way start, Way end, List<Way> addWays) {
+    this.start = start;
+    this.end = end;
+    this.addWays = addWays;
+  }
+
+  public void carryOut(Relation relation) {
+    List<RelationMember> newMembers = relation.getMembers();
+    int index = 0;
+    while (!newMembers.get(index).refersTo(start))
+      ++index;
+    ++index;
+    while (index < newMembers.size() && !newMembers.get(index).refersTo(end))
+      newMembers.remove(index);
+    for (Way w : addWays)
+      newMembers.add(index, new RelationMember(null, w));
+    
+    Relation newRelation = new Relation(relation);
+    newRelation.setMembers(newMembers);
+    ChangeCommand change = new ChangeCommand(relation, newRelation);
+    UndoRedoHandler.getInstance().add(change);
+  }
+  
+}
