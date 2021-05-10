68f10c83ea3f5372a712593c172609394227ab50
Archie Wheeler <archiewheeler@pm.me>
2021-05-10 21:27:47 +0100

    Remove multiple averaging methods
    Only the divide and conquer method is needed.

--- src/busroutemaintenance/AverageTracksAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package busroutemaintenance;
-
-import org.openstreetmap.josm.data.gpx.GpxData;
-import org.openstreetmap.josm.io.IllegalDataException;
-
-public interface AverageTracksAlgorithm {
-  public GpxData averageTracks(GpxData data) throws IllegalDataException;
-}
